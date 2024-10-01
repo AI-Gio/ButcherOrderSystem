90553f8010a6b1ea86aa8f4d21f1b8f368c9931b
AI-Gio <wreckitsnoofs@gmail.com>
2024-10-01 10:25:00 +0200

    Deleted import

--- app.js
+++ app.js
@@ -47,10 +47,6 @@ if ('serviceWorker' in navigator) {
     });
   }
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
